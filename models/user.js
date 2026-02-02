825cf8784ad5763fbdee7eb3a14c3c8d26b036d6
Adil Khan <adilkhan@Adils-MacBook-Air.local>
2026-02-01 20:08:04 -0600

    initialAuthCommit

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +17,36 @@ const userSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Wrong email format",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error("Incorrect password or email"));
+    }
+
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error("Incorrect password or email"));
+      }
+
+      return user;
+    });
+  });
+};
 
 module.exports = mongoose.model("user", userSchema);
